269948a7c63e6d3feb28563cbf730844001d216a
M.N <c0a22092cb@edu.teu.ac.jp>
2023-06-20 14:19:05 +0900

    練習問題1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,7 @@
+import random
 import sys
 import pygame as pg
+
 
 
 WIDTH, HEIGHT = 1600, 900
@@ -11,18 +13,28 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bd_img= pg.surface((20, 20)) #直径20の円
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    x = random.randint(0, WIDTH) 
+    y = random.randint(0, HEIGHT)
+    bd_rct = bg_img.get_rect()
+    #爆弾surface(bg_img)爆弾Rect(bd_rct)から抽出
+
+    bd_rct.center = x,y #爆弾Rectの中心座標
+
     clock = pg.time.Clock()
     tmr = 0
-    enn = pg.surface((20,20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
-    enn.set_colorkey((0, 0, 0))
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+            
+        
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
