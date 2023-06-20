45e4e6aa21874ede67e07ef3ec67a1baba781ea8
M.N <c0a22092cb@edu.teu.ac.jp>
2023-06-20 14:48:20 +0900

    練習問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,7 +5,12 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
-
+Moving ={
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -13,16 +18,18 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = bg_img.koukaton_rect()
+    #こうかとんsurface(bg_img)をこうかとんRect(bd_rct)から抽出
+    kk_rct.center = 900, 400
     bd_img= pg.surface((20, 20)) #直径20の円
     bd_img.set_colorkey((0, 0, 0))
     pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
     x = random.randint(0, WIDTH) 
     y = random.randint(0, HEIGHT)
     bd_rct = bg_img.get_rect()
-    #爆弾surface(bg_img)爆弾Rect(bd_rct)から抽出
-    vx, vy = +5, +5
-
-    bd_rct.center = x,y #爆弾Rectの中心座標
+    #爆弾surface(bg_img)を爆弾Rect(bd_rct)から抽出
+    bd_rct.center = x,y  #爆弾の中心座標を連数で指定
+    vx, vy = +5, +5 #爆弾の移動速度
 
     clock = pg.time.Clock()
     tmr = 0
@@ -32,10 +39,17 @@ def main():
             if event.type == pg.QUIT: 
                 return
             
-        
+        key_list = pg.key.get_pressed()
+        sum_mv = [0, 0] #合計移動量
+        for k, mv in Moving.items():
+            if key_list[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
+
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        bd_rct(vx, vy)
+        bd_rct(vx, vy) #爆弾の位置指定
         screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
